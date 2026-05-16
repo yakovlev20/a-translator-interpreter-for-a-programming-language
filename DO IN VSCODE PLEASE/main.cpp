3b761eb08d7ee64f7a2b8cfeec9271bf01a6bc07
solovozer <akvegetacocatrada3@gmail.com>
2026-05-16 11:00:21 +0700

    a

---
+++ DO IN VSCODE PLEASE/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "tokentype.h"
+
+using namespace std;
+
+class Token {
+public:
+    TokenType type;
+    string value;
+    Token(TokenType t, const string& v) : type(t), value(v) {}
+
+}
+
+
+class Lexer
+{
+public:
+
+    vector<string> tokenize(string code)
+    {
+        do {
+
+        } while 
+    }
+
+private:
+    string code;
+};
+
+Lexer lexer;
+
+int main()
+{
+    const string code = "begin { int x = 10; x = x + 1; out(x);}";
+    vector<string> tokens = lexer.tokenize(code);
+    
+}
