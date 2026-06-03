16cba29c4d35dbd2b3aa1a9e31b6b80ca62c7741
solovozer <akvegetacocatrada3@gmail.com>
2026-06-03 22:41:37 +0700

    a

--- COMBILYATOR/PARSER.h
+++ COMBILYATOR/PARSER.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include "Token.h"
+
+enum class ParserState {
+	A, L0, A1, X, X1, X2, X3,
+	E, S, H, G, Q, Q1, T, T1, F,
+};
+
+enum class Lemexe {
+
+};
+
+
+class Parser {
+	vector<Token*> tokens;
+
+public:
+	Parser(vector<Token*> tokens) { this->tokens = tokens; }
+	
+	string Parse() {
+		string RPN = "";
+		ParserState state = ParserState::A;
+
+		for (auto& token : tokens) {
+
+		}
+	}
+};
